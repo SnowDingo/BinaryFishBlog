742ab087de3d5e26382c577e244aeecf07bd9335
SnowDingo <Snowsable113@icloud.com>
2023-12-08 23:06:32 +0900

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   publicDir: './public',
   site: 'https://binary-fish.vercel.app/',
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [sitemap()]
 });
