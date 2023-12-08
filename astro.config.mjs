eb64b06547014681466e7ed7014256ba9380ccfd
SnowDingo <Snowsable113@icloud.com>
2023-12-08 19:12:59 +0900

    yay!!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    publicDir: './public',
-    site: 'https://binary-fish.vercel.app/',
-    
+  publicDir: './public',
+  site: 'https://binary-fish.vercel.app/',
+  output: "server",
+  adapter: vercel()
 });
