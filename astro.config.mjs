4fc35f2796f1b9b2a67e8f4837c633cad4643669
snowdingo <snowsable113@iloud.com>
2024-01-19 21:52:04 +0900

    クローラー

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   publicDir: './public',
-  site: 'https://binary-fish.vercel.app/',
+  site: 'https://binary-fish.vercel.app',
   output: "server",
   adapter: vercel(),
   integrations: [sitemap()]
